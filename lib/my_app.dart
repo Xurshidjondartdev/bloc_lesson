755a88a6c191a4d28776823f1c5de9def4441a1b
Xurshidjon Umarov <thebestxurshidjon@example.com>
2024-10-04 10:50:31 +0500

    next

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -6,9 +6,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PostScreen(),
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData.light(),
+      home: const PostScreen(),
     );
   }
 }
--- lib/setup.dart
+++ lib/setup.dart
@@ -1,8 +1,9 @@
 import 'package:bloc_lesson/config/setup_locator.dart';
+// import 'package:bloc_lesson/feature/post/post_bloc/simple_observer.dart';
 import 'package:flutter/material.dart';
 
-void setop() {
+void setup() {
   WidgetsFlutterBinding.ensureInitialized();
   setopLocator();
-  final observer = SimpleOb
+  // final observer = SimpleObserver();
 }
