ff31ccee3fc6ff2c41afe6b00ae7f3999646b4cf
Xurshidjon Umarov <thebestxurshidjon@example.com>
2024-10-04 11:15:00 +0500

    added folder

---
+++ lib/feature/post/bloc/post_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'post_event.dart';
+part 'post_state.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  PostBloc() : super(PostInitial()) {
+    on<PostEvent>((event, emit) {});
+  }
+}
