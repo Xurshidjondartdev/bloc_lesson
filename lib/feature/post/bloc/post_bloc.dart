65fa17514389d58231b2e48a1177ddd9586662bd
Xurshidjon Umarov <thebestxurshidjon@example.com>
2024-10-04 12:22:47 +0500

    PostBloc add

--- lib/feature/post/bloc/post_bloc.dart
+++ lib/feature/post/bloc/post_bloc.dart
@@ -15,7 +15,38 @@ class PostBloc extends Bloc<PostEvent, PostState> {
         final list = await repository.getAllPosts();
         emit(PostLoaded(postList: list));
       } catch (e) {
-        emit(PostError(postList: state.postList, errorText: "Smth went wrong"));
+        emit(PostError(postList: state.postList, errorText: "Smth went wrong: PostLoading... "));
+      }
+    });
+
+    on(<CreatePostEvent>(event, emit) async {
+      try {
+        emit(PostLoading(postList: state.postList));
+        await repository.createPost(post: PostModel(title: event.title, body: event.body));
+      } catch (e) {
+        emit(PostError(postList: state.postList, errorText: "Smth went wrong:  CreatePostEvent..."));
+      }
+    });
+
+    on<UpdatePostEvent>((event, emit) async {
+      try {
+        emit(PostLoading(postList: state.postList));
+        await repository.updatePost(
+            post: PostModel(id: int.parse(event.id), body: event.body, title: event.title));
+        emit(PostLoaded(postList: state.postList));
+      } catch (e) {
+        emit(PostError(postList: state.postList, errorText: "Smth went wrong:  UpdatePostEvent..."));
+      }
+    });
+
+    on<DeletePostEvent>((event, emit) async {
+      try {
+        emit(PostLoading(postList: state.postList));
+        await repository.deletePost(id: event.id);
+        emit(PostLoaded(postList: state.postList));
+        add(FetchPostEvent());
+      } catch (e) {
+        emit(PostError(postList: state.postList, errorText: "Smth went wrong:  DeletePostEvent..."));
       }
     });
   }
